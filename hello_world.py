e62184a7ea8c58e2554c5cc2fdb3d36ea30183f8
Matthew Owen <matthew.owen@consilium.technology>
2023-03-21 08:53:26 +1030

    add submodules test

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,16 @@
-from subtrees.hello_world import hello_world, hello_world_string
+import subtrees.hello_world as subtree
+import submodules.hello_world as submodule
 
 def main():
-    hello_world()
-    print(hello_world_string())
+    subtree_test()
+
+def subtree_test():
+    subtree.hello_world()
+    print(subtree.hello_world_string())
+
+def submodule_test():
+    submodule.hello_world()
+    print(submodule.hello_world_string())
 
 if __name__ == "__main__":
     main()
